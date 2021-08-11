6ad37e80baadc462723fc4c3c7c3cec0c1c88dac
jviteMx <64880840+jviteMx@users.noreply.github.com>
2021-08-10 22:21:21 -0600

    Sample output
    This files represent the output of each part of the program except the plots

--- Work_dir/tempCodeRunnerFile.py
+++ Work_dir/tempCodeRunnerFile.py
@@ -1,7 +1,1 @@
-for i in range(int(fileLenght)): 
-               
-        main(epochTime,i)
-        #x=threading.Thread(target=clean(fileName,i))
-        #os.system('python H:\MSc2\Work_dir\dataHandler.py')
-        #y=threading.Thread(target=computeAverages(output,i))        
-        k=i
+if __name__ == '__main__': 
